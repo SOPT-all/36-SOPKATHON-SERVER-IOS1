21efbf1667e2049ea3143b1f6d34f6cc44ed7d8e
ssangbaam <tkdqja2196@naver.com>
2025-05-15 22:26:04 +0900

    프로젝트 초기 설정

---
+++ src/main/java/sopt36/sopkathon/server_ios1/ServerIos1Application.java
@@ -0,0 +1,13 @@
+package sopt36.sopkathon.server_ios1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerIos1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerIos1Application.class, args);
+	}
+
+}
---
+++ src/test/java/sopt36/sopkathon/server_ios1/ServerIos1ApplicationTests.java
@@ -0,0 +1,13 @@
+package sopt36.sopkathon.server_ios1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerIos1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
